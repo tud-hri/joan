f288656fcb3f44f53fe87d2eef6891c098dee3e6
Joris Giltay <jorisgiltay@MacBook-Pro-van-Joris.local>
2020-03-17 14:50:16 +0100

    first iteration FDCA

--- modules/feedbackcontroller/action/feedbackcontroller.py
+++ modules/feedbackcontroller/action/feedbackcontroller.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore
 from PyQt5 import QtCore, QtWidgets, uic
 import os
+from process import Control
 
 class Basecontroller():
     def __init__(self):
@@ -28,10 +29,46 @@ class Manualcontrol(Basecontroller):
 
 class FDCAcontrol(Basecontroller): #NOG NIET AF
     def __init__(self, FeedbackcontrollerWidget):
+        self._parentWidget = FeedbackcontrollerWidget
         newtab = uic.loadUi(uifile = os.path.join(os.path.dirname(os.path.realpath(__file__)),"FDCA.ui"))
         FeedbackcontrollerWidget.widget.tabWidget.addTab(newtab,'FDCA')
         self.data = {}
 
+    def getNews(self,):
+        self._parentWidget.readNews('modules.feedbackcontroller.widget.feedbackcontroller.FeedbackcontrollerWidget')
+
     def process(self):
         self.data['FDCA Data Dingetje'] = 'hey hallo ik ben awesome'
+        
+        # self.Error = self.Error_Calc_wrt_HCR(self.T_aheadFF)
+
+        # ## GAINS  (FDCA as in SIMULINK)
+        # SWAngle_FB = self.SoHFFunc(self.K_y,self.K_psi,self.SoHF,self.Error[0],self.Error[1])
+         
+        # SWAngle_FFdes = self.FeedForwardController(0)
+          
+        # SWAngle_FF = self.LoHSFunc(self.LoHS,SWAngle_FFdes)
+
+        # SWAngleFB_FFDes = SWAngle_FB + SWAngle_FFdes #in radians
+
+        # SWAngle_Current = math.radians(self.Angle)
+
+        # delta_SW = SWAngleFB_FFDes - SWAngle_Current
+
+        # SWAngle_FF_FB = SWAngle_FF + SWAngle_FB
+
+        # Torque_LoHA = self.LoHAFunc(self.LoHA, delta_SW) #Torque resulting from feedback
+              
+        # K_stiffnessDeg = 40 # 40mNm/deg [DEGREES! CONVERT TO RADIANS!!]
+
+        # K_stiffnessRad = K_stiffnessDeg  * (math.pi/180)
+
+        # Torque_FF_FB = self.InverseSteeringDyn(SWAngle_FF_FB,K_stiffnessRad)
+
+        # Torque_FDCA = Torque_LoHA + Torque_FF_FB
+
+        # intTorque = int(round(Torque_FDCA*1000))
+
+        # TorqueBytes = int.to_bytes(intTorque,2, byteorder = 'little',signed= True)
+
         return self.data
