649e9e4f247a6b5002d5d6b0d89a84ba093e0cca
Joris Giltay <jorisgiltay@192.168.0.100>
2020-03-19 15:34:52 +0100

    trajgenstates

---
+++ modules/trajectoryrecorder/action/states.py
@@ -0,0 +1,14 @@
+from process import State, MasterStates, translate
+
+class TrajectorygeneratorStates(MasterStates):
+    # SensoDrive states
+    TRAJECTORYGENERATOR                  = State(500, translate('TemplateState', 'Template State'), -1,150)
+    
+
+    def __init__(self, *args, **kwargs):
+        MasterStates.__init__(self) 
+
+        #for state in self.states:
+        #    print('in TemplateStates', state, self.states[state])
+       
+    
