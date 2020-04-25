efd0ee7b4e674c49303b95713abebba826fc5efc
niek <niekbeckers@gmail.com>
2020-04-25 17:32:34 +0200

    First commit experimentmanager ui

--- modules/experimentmanager/dialog/experimentmanagerdialog.py
+++ modules/experimentmanager/dialog/experimentmanagerdialog.py
@@ -13,3 +13,12 @@ from modules.experimentmanager.action.experimentmanageraction import ExperimentM
 class ExperimentManagerDialog(JoanModuleDialog):
     def __init__(self, module_action: JoanModuleAction, master_state_handler, parent=None):
         super().__init__(module=JOANModules.EXPERIMENT_MANAGER, module_action=module_action, master_state_handler=master_state_handler, parent=parent)
+
+        self.module_widget.btn_initialize_condition.clicked.connect(self.initialize_condition)
+
+    def initialize_condition(self):
+        '''Initialize JOAN for the chosen condition'''
+        condition_nr = self.module_widget.combobox_conditions.currentIndex()
+
+        if condition_nr is not -1:
+            print('settings for condition')
