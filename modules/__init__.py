f2b9f9681985c4a928fae9a91c27319c2aaa1b2e
niek <niekbeckers@gmail.com>
2020-04-30 21:49:51 +0200

    Further clean up of master branch

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,1 @@
 # relative import used by sibling packages
-# action classes, belonging to widgets (within the same module directory) are imported in the widget-class
-
-# from .datarecorder.dialog.datarecorderdialog import DatarecorderDialog
-# from .hardwaremanager.dialog.hardwaremanagerdialog import HardwaremanagerDialog
-# # from .feedbackcontroller.widget.feedbackcontroller import FeedbackcontrollerWidget
-# from .carlainterface.dialog.carlainterfacedialog import CarlainterfaceDialog
-# from .trajectoryrecorder.widget.trajectoryrecorder import TrajectoryrecorderWidget
