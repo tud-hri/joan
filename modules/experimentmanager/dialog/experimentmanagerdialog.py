57a62ca718fa15b2159b77d42d602861cdcf230c
André van der Kraan <a.a.m.vanderkraan@tudelft.nl>
2020-05-29 08:45:42 +0200

    state change added when loading experiment

--- modules/experimentmanager/dialog/experimentmanagerdialog.py
+++ modules/experimentmanager/dialog/experimentmanagerdialog.py
@@ -19,6 +19,9 @@ class ExperimentManagerDialog(JoanModuleDialog):
         self.module_widget.btn_initialize_condition.clicked.connect(
             lambda idx: self.module_action.initialize_condition(self.module_widget.combobox_conditions.currentIndex())
         )
+        self.module_widget.btn_initialize_condition.clicked.connect(
+            lambda idx: self.initialize_condition()
+        )
 
         self.file_menu.addAction('Load experiment', self.process_menu_load_experiment)
 
@@ -41,3 +44,27 @@ class ExperimentManagerDialog(JoanModuleDialog):
         """process the experiment conditions"""
         self.module_widget.combobox_conditions.clear()
         self.module_widget.combobox_conditions.addItems(self.module_action.get_experiment_conditions())
+
+    def initialize_condition(self):
+        """
+        This function is called before the module is started
+        """
+        try:
+            self.module_action.module_state_handler.request_state_change(ExperimentManagerStates.INIT.INITIALIZED)
+        except RuntimeError:
+            return False
+
+    def handle_module_state(self, state):
+        """
+        Handle the state transition by updating the status label and have the
+        GUI reflect the possibilities of the current state.
+        """
+        try:
+            state_as_state = self.module_action.module_state_handler.get_state(state)  # ensure we have the State object (not the int)
+            # update the state label
+            self.state_widget.lbl_module_state.setText(state_as_state.name)
+            self.module_widget.repaint()
+
+        except Exception as inst:
+            print(inst)
+
