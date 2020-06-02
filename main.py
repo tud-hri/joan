78f71af4f1801b30efea66fdb7ab8de79a695223
jnpgiltay <j.n.p.giltay@tudelft.nl>
2020-06-02 14:40:04 +0200

    adjusted millis

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ if __name__ == '__main__':
     JOANHQWINDOW.show()
 
     # adding modules (instantiates them too)
-    JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, millis=50)
+    JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, millis=10)
     JOANHQACTION.add_module(JOANModules.HARDWARE_MANAGER, millis=2)
     JOANHQACTION.add_module(JOANModules.TEMPLATE)
     JOANHQACTION.add_module(JOANModules.DATA_RECORDER)
-    JOANHQACTION.add_module(JOANModules.STEERING_WHEEL_CONTROL, millis=2)
+    JOANHQACTION.add_module(JOANModules.STEERING_WHEEL_CONTROL, millis=10)
 
     # add the EXPERIMENT_MANAGER last because this one collects settings from JOAN modules that are already loaded
     JOANHQACTION.add_module(JOANModules.EXPERIMENT_MANAGER)
