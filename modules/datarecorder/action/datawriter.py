8a3adac161720205dd3d6f21e1918d888be4bd1d
Olger Siebinga <o.siebinga@fleetcleaner.com>
2020-06-02 16:18:02 +0200

    [BUGFIX]: old style settings reference in datarecorder prevented starting of that module

--- modules/datarecorder/action/datawriter.py
+++ modules/datarecorder/action/datawriter.py
@@ -27,7 +27,7 @@ class DataWriter(threading.Thread):
             except:
                 pass
             for key in latest_news:
-                if self.settings_dict['data'][channel][key] is True:
+                if self.settings.variables_to_save[channel][key] is True:
                     #row.append('%s.%s' % (channel.split('.')[1], key))
                     row.append('%s.%s' % (channel, key))
         self.columnnames(row)
@@ -41,7 +41,7 @@ class DataWriter(threading.Thread):
             except:
                 pass
             for key in latest_news:
-                if self.settings_dict['data'][channel][key] is True:
+                if self.settings.variables_to_save[channel][key] is True:
                     #row.update({'%s.%s' % (channel.split('.')[1], key): latest_news[key]})
                     row.update({'%s.%s' % (channel, key): latest_news[key]})
         return row
