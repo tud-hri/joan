0b6e7ca92542e4d80c10d7a0dca59e0d8cc3cd11
niek <niekbeckers@gmail.com>
2020-06-15 11:11:53 +0200

    Added SWController, removed FDcontroller and trajectoryrecorder

--- modules/joanmodules.py
+++ modules/joanmodules.py
@@ -14,9 +14,7 @@ class JOANModules(enum.Enum):
     HARDWARE_MANAGER = 1
     CARLA_INTERFACE = 2
     EXPERIMENT_MANAGER = 3
-    #FEED_BACK_CONTROLLER = 4
-    #TRAJECTORY_RECORDER = 5
-    
+    STEERING_WHEEL_CONTROL = 4
 
     @property
     def action(self):
@@ -26,29 +24,22 @@ class JOANModules(enum.Enum):
         from modules.hardwaremanager.action.hardwaremanageraction import HardwaremanagerAction
         from modules.carlainterface.action.carlainterfaceaction import CarlainterfaceAction
         from modules.experimentmanager.action.experimentmanageraction import ExperimentManagerAction
-    #    from modules.trajectoryrecorder.action.trajectoryrecorder import TrajectoryrecorderAction
-    #    from modules.feedbackcontroller.action.feedbackcontroller import FeedbackcontrollerAction
-        
+    
         return {JOANModules.TEMPLATE: TemplateAction,
                 JOANModules.DATA_RECORDER: DatarecorderAction,
                 JOANModules.STEERING_WHEEL_CONTROL: SteeringWheelControlAction,
                 JOANModules.HARDWARE_MANAGER: HardwaremanagerAction,
                 JOANModules.CARLA_INTERFACE: CarlainterfaceAction,
                 JOANModules.EXPERIMENT_MANAGER: ExperimentManagerAction}[self]
-    #            JOANModules.TRAJECTORY_RECORDER: TrajectoryrecorderAction,
-    #            JOANModules.FEED_BACK_CONTROLLER: FeedbackcontrollerAction}[self]
 
     @property
     def dialog(self):
         from modules.template.dialog.templatedialog import TemplateDialog
         from modules.datarecorder.dialog.datarecorderdialog import DatarecorderDialog
         from modules.steeringwheelcontrol.dialog.steeringwheelcontroldialog import SteeringWheelControlDialog
-        from modules.trajectoryrecorder.widget.trajectoryrecorder import TrajectoryrecorderWidget
         from modules.hardwaremanager.dialog.hardwaremanagerdialog import HardwaremanagerDialog
         from modules.carlainterface.dialog.carlainterfacedialog import CarlainterfaceDialog
         from modules.experimentmanager.dialog.experimentmanagerdialog import ExperimentManagerDialog
-    #    from modules.trajectoryrecorder.widget.trajectoryrecorder import TrajectoryrecorderWidget
-    #    from modules.feedbackcontroller.widget.feedbackcontroller import FeedbackcontrollerWidget
         
         return {JOANModules.TEMPLATE: TemplateDialog,
                 JOANModules.DATA_RECORDER: DatarecorderDialog,
@@ -56,29 +47,22 @@ class JOANModules(enum.Enum):
                 JOANModules.HARDWARE_MANAGER: HardwaremanagerDialog,
                 JOANModules.CARLA_INTERFACE: CarlainterfaceDialog,
                 JOANModules.EXPERIMENT_MANAGER: ExperimentManagerDialog}[self]
-    #            JOANModules.TRAJECTORY_RECORDER: TrajectoryrecorderWidget,
-    #            JOANModules.FEED_BACK_CONTROLLER: FeedbackcontrollerWidget}[self]
 
     @property
     def states(self):
         from modules.template.action.states import TemplateStates
         from modules.datarecorder.action.states import DatarecorderStates
-        from modules.trajectoryrecorder.action.states import TrajectoryrecorderStates
         from modules.hardwaremanager.action.states import HardwaremanagerStates
         from modules.carlainterface.action.states import CarlainterfaceStates
         from modules.experimentmanager.action.states import ExperimentManagerStates
-    #    from modules.trajectoryrecorder.action.states import TrajectoryrecorderStates
-    #    from modules.feedbackcontroller.action.states import FeedbackcontrollerStates
+        from modules.steeringwheelcontrol.action.states import SteeringWheelControlStates
         
         return {JOANModules.TEMPLATE: TemplateStates,
                 JOANModules.DATA_RECORDER: DatarecorderStates,
                 JOANModules.STEERING_WHEEL_CONTROL: SteeringWheelControlStates,
-                JOANModules.TRAJECTORY_RECORDER: TrajectoryrecorderStates,
                 JOANModules.HARDWARE_MANAGER: HardwaremanagerStates,
                 JOANModules.CARLA_INTERFACE: CarlainterfaceStates,
                 JOANModules.EXPERIMENT_MANAGER: ExperimentManagerStates}[self]
-    #            JOANModules.TRAJECTORY_RECORDER: TrajectoryrecorderStates,
-    #            JOANModules.FEED_BACK_CONTROLLER: FeedbackcontrollerStates}[self]
 
     @property
     def ui_file(self):
@@ -86,10 +70,9 @@ class JOANModules(enum.Enum):
         return {JOANModules.TEMPLATE: os.path.join(path_to_modules, "template/dialog/templatewidget.ui"),
                 JOANModules.DATA_RECORDER: os.path.join(path_to_modules, "datarecorder/dialog/datarecorder.ui"),
                 JOANModules.CARLA_INTERFACE: os.path.join(path_to_modules, "carlainterface/dialog/carlainterface.ui"),
+                JOANModules.STEERING_WHEEL_CONTROL: os.path.join(path_to_modules, "steeringwheelcontrol/dialog/steeringwheelcontrol.ui"),
                 JOANModules.HARDWARE_MANAGER: os.path.join(path_to_modules, "hardwaremanager/dialog/hardwaremanager.ui"),
                 JOANModules.EXPERIMENT_MANAGER: os.path.join(path_to_modules, "experimentmanager/dialog/experimentmanager.ui")}[self]
-    #            JOANModules.TRAJECTORY_RECORDER: os.path.join(path_to_modules, "trajectoryrecorder/widget/trajectoryrecorder.ui"),
-    #            JOANModules.FEED_BACK_CONTROLLER: os.path.join(path_to_modules, "feedbackcontroller/widget/feedbackcontroller.ui")}[self]
 
     def __str__(self):
         return {JOANModules.TEMPLATE: 'Template',
@@ -98,5 +81,3 @@ class JOANModules(enum.Enum):
                 JOANModules.HARDWARE_MANAGER: 'Hardware Communication',
                 JOANModules.CARLA_INTERFACE: 'Carla Interface',
                 JOANModules.EXPERIMENT_MANAGER: 'Experiment Manager'}[self]
-    #            JOANModules.TRAJECTORY_RECORDER: 'Trajectory Recorder',
-    #            JOANModules.FEED_BACK_CONTROLLER: 'Feed Back Controller'}[self]
