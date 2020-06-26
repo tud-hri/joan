7a25c30ab47def9e2219185122f694f82427bafa
André van der Kraan <a.a.m.vanderkraan@tudelft.nl>
2020-06-26 17:15:43 +0200

    first step to datarecorder recursives news settings from other modules

--- process/status.py
+++ process/status.py
@@ -13,15 +13,16 @@ class Status:
         if not cls.instance:
             cls.instance = object.__new__(Status)
             # start deprecated 
-            cls._master_states = MasterStates()
-            cls._master_state_handler = StateHandler(first_state=MasterStates.VOID, states_dict=cls._master_states.get_states())
-            cls._module_state_packages = {}
+            #cls._master_states = MasterStates()
+            #cls._master_state_handler = StateHandler(first_state=MasterStates.VOID, states_dict=cls._master_states.get_states())
+            #cls._module_state_packages = {}
             # end deprecated
 
             cls._module_state_machines = {}  # enum style (e.g. JOANModules.HARDWARE_MANAGER: State.READY)
         return cls.instance
 
     
+    '''
     # will become deprecated after enum
     def register_module_state_package(self, module: JOANModules, module_state_package):
         self._module_state_packages.update({module: module_state_package})
@@ -32,7 +33,7 @@ class Status:
             return self._module_state_packages[module]
         except KeyError:
             return {}
-    
+    '''
 
     def update_state_machine(self, module: JOANModules, module_state_machine):
         " set state_machine of every module in the singleton, state_machine holds the current state for his/her own module "
@@ -51,6 +52,8 @@ class Status:
         except KeyError:
             return None
 
+
+    '''
     # will become deprecated after enum
     @property
     def all_module_state_packages(self):
@@ -60,3 +63,4 @@ class Status:
     @property
     def all_module_state_package_keys(self):
         return self._module_state_packages.keys()
+    '''
