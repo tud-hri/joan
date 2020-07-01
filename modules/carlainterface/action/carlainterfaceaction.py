c49bb48458de436e2e885d4091162cbceb6e078a
Joris Giltay <jorisgiltay@gmail.com>
2020-07-01 16:45:45 +0200

    first iteration of traffic that follows recorded trajectories with PD Control

--- modules/carlainterface/action/carlainterfaceaction.py
+++ modules/carlainterface/action/carlainterfaceaction.py
@@ -6,12 +6,16 @@ from modules.joanmodules import JOANModules
 from process.joanmoduleaction import JoanModuleAction
 from process.statesenum import State
 from process.status import Status
+from utils.utils import Biquad
 
 import time
 import random
 import os
 import sys
 import glob
+import pandas as pd
+import numpy as np
+import math
 
 msg_box = QMessageBox()
 msg_box.setTextFormat(QtCore.Qt.RichText)
@@ -151,6 +155,9 @@ class CarlainterfaceAction(JoanModuleAction):
                 for items in self.vehicles:
                     if items.spawned:
                         items.apply_control(self._data_from_hardware)
+                for items in self.traffic_vehicles:
+                    if items.spawned:
+                        items.process()
             except Exception as inst:
                 print('Could not apply control', inst)
         else:
@@ -268,6 +275,9 @@ class CarlainterfaceAction(JoanModuleAction):
                 vehicle.get_available_inputs()
                 vehicle.get_available_controllers()
             self.state_machine.request_state_change(State.READY, "You can now add vehicles and start the module")
+
+            for traffic in self.traffic_vehicles:
+                traffic.stop_traffic()
         except RuntimeError:
             return False
         return super().stop()
@@ -446,6 +456,20 @@ class Carlavehicle():
 class Trafficvehicle():
     def __init__(self, CarlainterfaceAction, traffic_vehicle_nr, nr_spawn_points, tags):
         "Init traffic vehicle here"
+
+        self._bq_filter_heading = Biquad()
+        self._bq_filter_velocity = Biquad()
+
+        self._t2 = 0
+
+        self._controller_error = np.array([0.0, 0.0, 0.0, 0.0])
+        self.error_static_old = np.array([0.0, 0.0])
+
+        self._current_trajectory_name = ''
+        curpath = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.dirname(os.path.dirname(curpath))
+        self._path_trajectory_directory = os.path.join(path, 'steeringwheelcontrol/action/swcontrollers/trajectories/')
+
         self.traffic_vehicle_nr = traffic_vehicle_nr
         self.module_action = CarlainterfaceAction
         self._traffic_vehicle_tab = uic.loadUi(uifile=os.path.join(os.path.dirname(os.path.realpath(__file__)), "trafficvehicletab.ui"))
@@ -455,8 +479,19 @@ class Trafficvehicle():
         self._traffic_vehicle_tab.btn_destroy.clicked.connect(self.destroy_car)
         self._traffic_vehicle_tab.btn_remove_traffic_agent.clicked.connect(self.remove_traffic_agent)
 
+        self._traffic_vehicle_tab.btn_update_hcr_list.clicked.connect(self.update_trajectory_list)
+        self._traffic_vehicle_tab.combo_box_traffic_trajectory.currentIndexChanged.connect(self.load_trajectory)
+
         self._traffic_vehicle_tab.spin_spawn_points.setRange(0, nr_spawn_points)
         self._traffic_vehicle_tab.spin_spawn_points.lineEdit().setReadOnly(True)
+
+        self.update_trajectory_list()
+
+        self.w_lat = 1
+        self.w_heading = 2
+        self.k_p = 6
+        self.k_d = 2.5
+
 
         for item in tags:
             self._traffic_vehicle_tab.combo_car_type.addItem(item)
@@ -477,7 +512,7 @@ class Trafficvehicle():
 
     def spawn_car(self):
         self._BP = random.choice(self.module_action.vehicle_bp_library.filter("vehicle." + str(self.traffic_vehicle_tab.combo_car_type.currentText())))
-        self._control = carla.VehicleControl()
+        self._traffic_control = carla.VehicleControl()
         try:
             spawnpointnr = self._traffic_vehicle_tab.spin_spawn_points.value()-1
             self.spawned_traffic_vehicle = self.module_action.world.spawn_actor(self._BP, self.module_action.spawnpoints[spawnpointnr])
@@ -485,6 +520,7 @@ class Trafficvehicle():
             self._traffic_vehicle_tab.btn_destroy.setEnabled(True)
             self._traffic_vehicle_tab.spin_spawn_points.setEnabled(False)
             self._traffic_vehicle_tab.combo_car_type.setEnabled(False)
+            self.traffic_velocity = self.spawned_traffic_vehicle.get_velocity()
             #self.get_available_inputs()
             self._spawned = True
         except Exception as inst:
@@ -517,8 +553,146 @@ class Trafficvehicle():
 
         self.module_action.traffic_vehicles.remove(self)
 
-class TrafficPDController():
-    def __init__(self):
-        print('constructed')
+    def load_trajectory(self):
+        """Load HCR trajectory"""
+        fname = self._traffic_vehicle_tab.combo_box_traffic_trajectory.itemText(self._traffic_vehicle_tab.combo_box_traffic_trajectory.currentIndex())
+
+        if fname != self._current_trajectory_name:
+            # fname is different from _current_hcr_name, load it!
+            try:
+                tmp = pd.read_csv(os.path.join(self._path_trajectory_directory, fname))
+                self._trajectory = tmp.values
+                # TODO We might want to do some checks on the trajectory here.
+                self._current_trajectory_name = fname
+            except OSError as err:
+                print('Error loading HCR trajectory file: ', err)
+
+    def update_trajectory_list(self):
+        """Check what trajectory files are present and update the selection list"""
+        # get list of csv files in directory
+        if not os.path.isdir(self._path_trajectory_directory):
+            os.mkdir(self._path_trajectory_directory)
+        files = [filename for filename in os.listdir(self._path_trajectory_directory) if filename.endswith('csv')]
+
+        self._traffic_vehicle_tab.combo_box_traffic_trajectory.clear()
+        self._traffic_vehicle_tab.combo_box_traffic_trajectory.addItems(files)
+
+        idx = self._traffic_vehicle_tab.combo_box_traffic_trajectory.findText(self._current_trajectory_name)
+        if idx != -1:
+            self._traffic_vehicle_tab.combo_box_traffic_trajectory.setCurrentIndex(idx)
+
+    def find_closest_node(self, node, nodes):
+        """find the node in the nodes list (trajectory)"""
+        nodes = np.asarray(nodes)
+        deltas = nodes - node
+        dist_squared = np.einsum('ij,ij->i', deltas, deltas)
+        return np.argmin(dist_squared)
+
+    def process(self):
+        try:
+            """Perform the controller-specific calculations"""
+            # get delta_t (we could also use 'millis' but this is a bit more precise)
+            t1 = time.time()
+            delta_t = t1 - self._t2
+
+            # extract data
+            car = self.spawned_traffic_vehicle
+            pos_car = np.array([car.get_location().x, car.get_location().y])
+            vel_car = np.array([car.get_velocity().x, car.get_velocity().y])
+            heading_car = car.get_transform().rotation.yaw
+            forward_vector = car.get_transform().rotation.get_forward_vector()
+            # vel_traffic = car.get_velocity()
+            vel_traffic = forward_vector * 60/3.6
+
+            # find static error and error rate:
+            error_static = self.error(pos_car, heading_car, vel_car)
+            error_rate = self.error_rates(error_static, self.error_static_old, delta_t)
+
+            # filter the error rate with biquad filter
+            error_lateral_rate_filtered = self._bq_filter_velocity.process(error_rate[0])
+            error_heading_rate_filtered = self._bq_filter_heading.process(error_rate[1])
+
+            # put errors in 1 variable
+            self._controller_error[0:2] = error_static
+            self._controller_error[2:] = np.array([error_lateral_rate_filtered, error_heading_rate_filtered])
+
+            # put error through controller to get sw torque out
+            self._sw_angle = self.pd_controller(self._controller_error)
+            print(self._sw_angle)
+            self._traffic_control.steer = self._sw_angle
+            self.spawned_traffic_vehicle.apply_control(self._traffic_control)
+            self.spawned_traffic_vehicle.set_velocity(vel_traffic)
 
 
+            # update variables
+            self.error_static_old = error_static
+            self._t2 = t1
+        except Exception as inst:
+
+            print(inst)
+
+    def error(self, pos_car, heading_car, vel_car=np.array([0.0, 0.0, 0.0])):
+        """Calculate the controller error"""
+        pos_car_future = pos_car + vel_car * 0.6  # linear extrapolation, should be updated
+
+
+        index_closest_waypoint = self.find_closest_node(pos_car_future, self._trajectory[:, 1:3])
+
+        if index_closest_waypoint >= len(self._trajectory) - 3:
+            index_closest_waypoint_next = 0
+        else:
+            index_closest_waypoint_next = index_closest_waypoint + 3
+
+        # calculate lateral error
+        pos_ref_future = self._trajectory[index_closest_waypoint, 1:3]
+        pos_ref_future_next = self._trajectory[index_closest_waypoint_next, 1:3]
+
+        vec_pos_future = pos_car_future - pos_ref_future
+        vec_dir_future = pos_ref_future_next - pos_ref_future
+
+        error_lat_sign = np.math.atan2(np.linalg.det([vec_dir_future, vec_pos_future]),
+                                       np.dot(vec_dir_future, vec_pos_future))
+
+        error_pos_lat = np.sqrt(vec_pos_future.dot(vec_pos_future))
+
+        if error_lat_sign < 0:
+            error_pos_lat = -error_pos_lat
+
+        # calculate heading error
+        heading_ref = self._trajectory[index_closest_waypoint, 6]
+
+        error_heading = -(math.radians(heading_ref) - math.radians(heading_car))
+
+        # Make sure you dont get jumps (basically unwrap the angle with a threshold of pi radians (180 degrees))
+        if error_heading > math.pi:
+            error_heading = error_heading - 2.0 * math.pi
+        if error_heading < -math.pi:
+            error_heading = error_heading + 2.0 * math.pi
+
+        return np.array([error_pos_lat, error_heading])
+
+    def error_rates(self, error, error_old, delta_t):
+        """Calculate the controller error rates"""
+
+        heading_error_rate = (error[0] - error_old[0]) / delta_t
+        velocity_error_rate = (error[1] - error_old[1]) / delta_t
+
+        return np.array([velocity_error_rate, heading_error_rate])
+
+    def pd_controller(self, error):
+        lateral_gain = self.w_lat * (self.k_p * error[0] + self.k_d * error[2])
+        heading_gain = self.w_heading * (self.k_p * error[1] + self.k_d* error[3])
+        #
+        total_gain = lateral_gain+ heading_gain
+        sw_angle = total_gain/450
+
+        return -sw_angle
+        #
+        # return int(torque_gain)
+
+    def stop_traffic(self):
+        car = self.spawned_traffic_vehicle
+        forward_vector = car.get_transform().rotation.get_forward_vector()
+        # vel_traffic = car.get_velocity()
+        vel_traffic = forward_vector *0
+        self.spawned_traffic_vehicle.set_velocity(vel_traffic)
