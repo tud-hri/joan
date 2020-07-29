0e3d23c9d791121fc1bd059579e1d221cf6fb91b
Olger Siebinga <o.siebinga@fleetcleaner.com>
2020-07-29 13:48:22 +0200

    removed some more deprecated code from experimentmanager action
    Former-commit-id: aa8ba589e72c46a5ced7c7e343eb595f01e739d1

--- modules/experimentmanager/action/experimentmanageraction.py
+++ modules/experimentmanager/action/experimentmanageraction.py
@@ -12,22 +12,11 @@ class ExperimentManagerAction(JoanModuleAction):
         self.data = {}
         self.write_news(news=self.data)
 
-        # set factory settings per module, must be called before self.write_default_experiment()
-        self._set_factory_settings_in_singleton()
-
         # create/get default experiment_settings
         self.my_file = os.path.join('.', 'default_experiment_settings.json')
         # First remove current file
         if os.path.exists(self.my_file):
             os.remove(self.my_file)
-        self.settings_object = ModuleSettings(file=self.my_file)
-        self.settings = self.settings_object.read_settings()
-        self.settings.update(self._get_attention_message())
-        self.share_settings(self.settings)
-
-        self.write_default_experiment()  # maybe used as an example for experiment conditions
-        self.experiment_settings = {}  # will contain experiment_settings
-        self.condition_names = []  # will contain condition_names for use in getting correct settings
 
     def do(self):
         """
