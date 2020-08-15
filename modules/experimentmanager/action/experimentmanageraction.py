0ab26184790b9e0b42693c6621a03f1708e7caef
niek <n.w.m.beckers@tudelft.nl>
2020-08-15 22:08:20 +0200

    condition set settings from dict.
    Former-commit-id: 0c6b3f996704bf73a0f3c53e4871edb1b80b6faf

--- modules/experimentmanager/action/experimentmanageraction.py
+++ modules/experimentmanager/action/experimentmanageraction.py
@@ -74,11 +74,9 @@ class ExperimentManagerAction(JoanModuleAction):
         # TODO: implement update_settings_from_dict function in each module settings class
         # apply base settings first, then condition settings
         for module, settings_dict in self.current_experiment.base_settings:
-            # self.singleton_settings.get_settings(module).update_settings_from_dict(settings_dict)
-            pass
+            self.singleton_settings.get_settings(module).set_from_loaded_dict(settings_dict)
 
         for module, settings_dict in condition.diff:
-            # self.singleton_settings.get_settings(module).update_settings_from_dict(settings_dict)
-            pass
+            self.singleton_settings.get_settings(module).set_from_loaded_dict(settings_dict)
 
         # TODO signals to nodules (transitions)
