126126edcfaadd71cda3295273b4ef42ea056c3e
Joris Giltay <j.n.p.giltay@tudelft.nl>
2020-10-16 15:33:16 +0200

    hardcoded not catching keyevents in mp prcocess
    Former-commit-id: 844475fb387812aa62f8e88f6b4b41803e0c6cd3

--- modules/hardwaremp/hardwaremp_process.py
+++ modules/hardwaremp/hardwaremp_process.py
@@ -8,6 +8,12 @@ class HardwareMPProcess(ModuleProcess):
     def __init__(self, module: JOANModules, time_step, news):
         super().__init__(module, time_step=time_step, news=news)
         self.shared_values = news.read_news(JOANModules.HARDWARE_MP)
+        self._throttle = False
+        self._brake = False
+        self._steer_left = False
+        self._steer_right = False
+        self._handbrake = False
+        self._reverse = False
 
         # print(self.bleh)
         # print(self.news)
@@ -23,7 +29,10 @@ class HardwareMPProcess(ModuleProcess):
 
 
     def do_function(self):
+        self._brake = True
         self.do()
+        print(self.shared_values.brake)
+
         pass
 
     def key_event(self, key):
@@ -32,24 +41,25 @@ class HardwareMPProcess(ModuleProcess):
         :param key:
         :return:
         """
+        print('joe')
         boolean_key_press_value = key.event_type == keyboard.KEY_DOWN
         int_key_identifier = QtGui.QKeySequence(key.name)[0]
 
-        if int_key_identifier == self.settings.throttle_key:
+        if int_key_identifier == 87:
             self._throttle = boolean_key_press_value
-        elif int_key_identifier == self.settings.brake_key:
+        elif int_key_identifier == 83:
             self._brake = boolean_key_press_value
-        elif int_key_identifier == self.settings.steer_left_key:
+        elif int_key_identifier == 65:
             self._steer_left = boolean_key_press_value
             if boolean_key_press_value:
                 self._steer_right = False
-        elif int_key_identifier == self.settings.steer_right_key:
+        elif int_key_identifier == 68:
             self._steer_right = boolean_key_press_value
             if boolean_key_press_value:
                 self._steer_left = False
-        elif int_key_identifier == self.settings.handbrake_key:
+        elif int_key_identifier == 32:
             self._handbrake = boolean_key_press_value
-        elif int_key_identifier == self.settings.reverse_key and boolean_key_press_value:
+        elif int_key_identifier == 82 and boolean_key_press_value:
             self._reverse = not self._reverse
 
     def do(self):
@@ -65,9 +75,9 @@ class HardwareMPProcess(ModuleProcess):
         """
         # Throttle:
         if self._throttle and self.shared_values.throttle < 1:
-            self.shared_values.throttle = self.shared_values.throttle + (0.05 * self.settings.throttle_sensitivity / 100)
+            self.shared_values.throttle = self.shared_values.throttle + (0.05 * 50 / 100)
         elif self.shared_values.throttle > 0 and not self._throttle:
-            self.shared_values.throttle = self.shared_values.throttle - (0.05 * self.settings.throttle_sensitivity / 100)
+            self.shared_values.throttle = self.shared_values.throttle - (0.05 * 50 / 100)
         elif self.shared_values.throttle < 0:
             self.shared_values.throttle = 0
         elif self.shared_values.throttle > 1:
@@ -75,25 +85,25 @@ class HardwareMPProcess(ModuleProcess):
 
         # Brake:
         if self._brake and self.shared_values.brake < 1:
-            self.shared_values.brake = self.shared_values.brake + (0.05 * self.settings.brake_sensitivity / 100)
+            self.shared_values.brake = self.shared_values.brake + (0.05 * 50 / 100)
         elif self.shared_values.brake > 0 and not self._brake:
-            self.shared_values.brake = self.shared_values.brake - (0.05 * self.settings.brake_sensitivity / 100)
+            self.shared_values.brake = self.shared_values.brake - (0.05 * 50/ 100)
         elif self.shared_values.brake < 0:
             self.shared_values.brake = 0
         elif self.shared_values.brake > 1:
             self.shared_values.brake = 1
 
         # Steering:
-        if self._steer_left and self.settings.max_steer >= self.shared_values.steering_angle >= self.settings.min_steer:
-            self.shared_values.steering_angle = self.shared_values.steering_angle - (self.settings.steer_sensitivity / 10000)
-        elif self._steer_right and self.settings.min_steer <= self.shared_values.steering_angle <= self.settings.max_steer:
-            self.shared_values.steering_angle = self.shared_values.steering_angle + (self.settings.steer_sensitivity / 10000)
-        elif self.shared_values.steering_angle > 0 and self.settings.auto_center:
-            self.shared_values.steering_angle = self.shared_values.steering_angle - (self.settings.steer_sensitivity / 10000)
-        elif self.shared_values.steering_angle < 0 and self.settings.auto_center:
-            self.shared_values.steering_angle = self.shared_values.steering_angle + (self.settings.steer_sensitivity / 10000)
+        if self._steer_left and 1.57 >= self.shared_values.steering_angle >= -1.57:
+            self.shared_values.steering_angle = self.shared_values.steering_angle - (50 / 10000)
+        elif self._steer_right and -1.57 <= self.shared_values.steering_angle <= 1.57:
+            self.shared_values.steering_angle = self.shared_values.steering_angle + (50 / 10000)
+        elif self.shared_values.steering_angle > 0:
+            self.shared_values.steering_angle = self.shared_values.steering_angle - (50 / 10000)
+        elif self.shared_values.steering_angle < 0:
+            self.shared_values.steering_angle = self.shared_values.steering_angle + (50 / 10000)
 
-        if abs(self.shared_values.steering_angle) < self.settings.steer_sensitivity / 10000:
+        if abs(self.shared_values.steering_angle) < 50 / 10000:
             self.shared_values.steering_angle = 0
 
         # Reverse
