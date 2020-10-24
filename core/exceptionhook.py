684acce63acd6d1064218718d0cbd17c0187dfb0
niek <n.w.m.beckers@tudelft.nl>
2020-10-24 21:20:09 +0200

    Fixed a merge error in exceptionhook.
    Former-commit-id: 75c475ff0646d342545f99448ad37e03948ce0a7

--- core/exceptionhook.py
+++ core/exceptionhook.py
@@ -6,28 +6,25 @@ import traceback
 from PyQt5 import QtWidgets, QtCore
 
 
-def exception_log_and_kill_hook(exctype, value, tb, joan_module):
-    """
-    Logs exception and kills the process
-    :param exctype: Exception type from from exc_info() in process
-    :param value: from exc_info() in process
-    :param tb: traceback from exc_info() in process
-    :param joan_module: module that had an exception, needed for log
-    :return:
-    """
+def exception_log_and_kill_hook(exctype, value, tb, joan_module, exception_event):
+
+    # trigger the event to transition the module to ERROR state
+    exception_event.set()
+
     print('exception occurred in run process of the %s module' % str(joan_module))
     traceback.print_exception(exctype, value, tb)
 
-    dialog_message = "An error occurred in the % s module. A log has been saved." % str(joan_module)
-
     #  save log
     try:
-        log_dir = os.getcwd() + '/crash_logs/'
+        log_dir = os.path.join(os.getcwd(),'crash_logs')
         if not os.path.isdir(os.path.dirname(log_dir)):
             os.makedirs(log_dir)
 
-        with open(log_dir + str(joan_module).replace(' ', '_') + '_' + time.strftime('%d-%m-%Y_%Hh%Mm%Ss') + '.txt', 'a') as file:
+        file_path = log_dir + os.path.sep + str(joan_module).replace(' ', '_') + '_' + time.strftime('%d-%m-%Y_%Hh%Mm%Ss') + '.txt'
+        with open(file_path, 'a') as file:
             traceback.print_exception(exctype, value, tb, file=file)
+
+        dialog_message = "An error occurred in the %s module. A log has been saved at %s." % (str(joan_module), file_path)
     except:  # if log saving fails here it's too far gone to attempt a rescue. Just give the user the opportunity to copy the error.
         dialog_message = "An error occurred in %s. \n \n WARNING: A log file could not be saved, but the stack trace can be copied from the terminal." % str(
             joan_module)
