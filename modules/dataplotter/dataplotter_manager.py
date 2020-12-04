c0bfd75f4478fcc7df553ded6accbd111b2ea2ec
Joris Giltay <jorisgiltay@gmail.com>
2020-12-04 16:54:09 +0100

    dataplotter docstrings and cleaned up code
    Former-commit-id: ab511a8602750c9e0f8b49faad76c7b667872155

--- modules/dataplotter/dataplotter_manager.py
+++ modules/dataplotter/dataplotter_manager.py
@@ -9,6 +9,7 @@ class DataPlotterManager(ModuleManager):
     """ Manages the datarecorder environment """
 
     def __init__(self, news, signals, time_step_in_ms=10, parent=None):
-        super().__init__(module=JOANModules.DATA_PLOTTER, news=news, signals=signals, time_step_in_ms=time_step_in_ms, parent=parent)
+        super().__init__(module=JOANModules.DATA_PLOTTER, news=news, signals=signals, time_step_in_ms=time_step_in_ms,
+                         parent=parent)
 
         self.state_machine.set_exit_action(State.INITIALIZED, self.module_dialog.apply_settings)
