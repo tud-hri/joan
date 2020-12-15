d0c2bf48bf20c35419840e6b7947838fa4a655b7
Joris Giltay <j.n.p.giltay@tudelft.nl>
2020-12-15 11:05:14 +0100

    Included all modules except controller plotter
    Former-commit-id: 38f6757bc6e1bd6203209ae8d7401c4a7997cfdb

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
     JOANHQACTION = HQManager()
 
     # adding modules (instantiates them too)
-    # JOANHQACTION.add_module(JOANModules.TEMPLATE, time_step_in_ms=100)
+    #JOANHQACTION.add_module(JOANModules.TEMPLATE, time_step_in_ms=100)
     JOANHQACTION.add_module(JOANModules.HARDWARE_MANAGER, time_step_in_ms=10)
-    # JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, time_step_in_ms=10)
+    JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, time_step_in_ms=10)
     JOANHQACTION.add_module(JOANModules.DATA_PLOTTER, time_step_in_ms=10)
     JOANHQACTION.add_module(JOANModules.HAPTIC_CONTROLLER_MANAGER, time_step_in_ms=10)
-    JOANHQACTION.add_module(JOANModules.CONTROLLER_PLOTTER, time_step_in_ms=500)
+    # JOANHQACTION.add_module(JOANModules.CONTROLLER_PLOTTER, time_step_in_ms=500)
     JOANHQACTION.add_module(JOANModules.DATA_RECORDER, time_step_in_ms=10)
     JOANHQACTION.add_module(JOANModules.EXPERIMENT_MANAGER, time_step_in_ms=500)
 
