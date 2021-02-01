995066319b06cfb5c98c206c8bbcbd471348bb7a
niek <n.w.m.beckers@tudelft.nl>
2021-02-01 12:00:20 +0100

    Commented CarlaInterface out in main.py
    Former-commit-id: 9569f7affb9301b2db0e5fc384c6c1069e92227d

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # adding modules (instantiates them too)
     #JOANHQACTION.add_module(JOANModules.TEMPLATE, time_step_in_ms=100)
     JOANHQACTION.add_module(JOANModules.HARDWARE_MANAGER, time_step_in_ms=10)
-    JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, time_step_in_ms=10)
+    # JOANHQACTION.add_module(JOANModules.CARLA_INTERFACE, time_step_in_ms=10)
     JOANHQACTION.add_module(JOANModules.DATA_PLOTTER, time_step_in_ms=10)
     JOANHQACTION.add_module(JOANModules.HAPTIC_CONTROLLER_MANAGER, time_step_in_ms=10)
     # JOANHQACTION.add_module(JOANModules.CONTROLLER_PLOTTER, time_step_in_ms=500)
